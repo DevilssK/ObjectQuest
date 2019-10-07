18607083e077a9a2cca3215d4be7db7961172766
Axel S <saxelsyeguillaume@gmail.com>
2019-10-07 10:57:18 +0200

    Spring init

---
+++ Object-Quest/src/main/java/fr/intech/Java9/Object/Quest/ObjectQuestApplication.java
@@ -0,0 +1,13 @@
+package fr.intech.Java9.Object.Quest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObjectQuestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObjectQuestApplication.class, args);
+	}
+
+}
